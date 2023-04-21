4ababc58d7296bc9433307568fe82dde6ff81a64
Prajyot Khandeparkar <prajyotpro@gmail.com>
2023-04-22 01:19:53 +0530

    file and folder naming, updated to only ts

--- src/controller/HomeController.ts
+++ src/controller/HomeController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
-import { Controller } from "../core/controller";
+import { BaseController } from "../core/BaseController";
 
-export class HomeController extends Controller {
+export class HomeController extends BaseController {
 
     static index(req:Request, res:Response) {
         return res.json('GET request to the homepage')
